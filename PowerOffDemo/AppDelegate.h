9f90744626462925879c2536376bcb6e2b7ccf1c
Garrett Richards <grichards@trulia.com>
2014-10-13 10:17:05 -0700

    initial commit of workspace

---
+++ PowerOffDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PowerOffDemo
+//
+//  Created by Garrett Richards on 10/13/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PowerOffDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PowerOffDemo
+//
+//  Created by Garrett Richards on 10/13/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
