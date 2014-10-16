e965ce65ad1f9140340b32d712001431e38d7fc8
Garrett Richards <garrettrichards@gmail.com>
2014-10-15 23:34:05 -0700

    removed code for shimmer

--- PowerOffDemo/GRSliderWithLabel.h
+++ PowerOffDemo/GRSliderWithLabel.h
@@ -8,12 +8,6 @@
 
 #import "GRSlider.h"
 
-//  http://stackoverflow.com/a/5710097/202064
-@interface UILabel (FSHighlightAnimationAdditions)
-- (void)setTextWithChangeAnimation:(NSString*)text;
-@end
-
-
 @interface GRSliderWithLabel : GRSlider
 @property(nonatomic, strong) UILabel *textLabel;
 @end
